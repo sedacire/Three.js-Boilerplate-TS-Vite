3730c8a99c64c15bb65aa9a3549eb1d89646b1c6
Erica Desrochers <erica@affordancestudio.com>
2025-10-01 06:22:35 -0400

    wip

--- src/main.ts
+++ src/main.ts
@@ -1,36 +1,26 @@
 import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
 import { RGBELoader } from 'three/addons/loaders/RGBELoader.js'
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
 import Stats from 'three/addons/libs/stats.module.js'
-import { Lensflare, LensflareElement } from 'three/addons/objects/Lensflare.js'
 
 const scene = new THREE.Scene()
 
-// const light = new THREE.SpotLight(undefined, Math.PI * 1000)
-// light.position.set(5, 5, 5)
-// light.angle = Math.PI / 16
-// light.castShadow = true
-// scene.add(light)
-
-// const helper = new THREE.SpotLightHelper(light)
-// scene.add(helper)
+const gridHelper = new THREE.GridHelper(100, 100)
+scene.add(gridHelper)
 
 new RGBELoader().load('img/venice_sunset_1k.hdr', (texture) => {
-  texture.mapping = THREE.EquirectangularReflectionMapping
-  scene.environment = texture
+	texture.mapping = THREE.EquirectangularReflectionMapping
+	scene.environment = texture
 	scene.background = texture
-	scene.backgroundBlurriness = 0.15
+	scene.backgroundBlurriness = 1
 })
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100)
-camera.position.set(1.5, 0.75, 2)
+camera.position.set(0.1, 1, 1)
 
 const renderer = new THREE.WebGLRenderer({ antialias: true })
-renderer.toneMapping = THREE.ACESFilmicToneMapping
-renderer.toneMappingExposure = 0.8
-renderer.shadowMap.enabled = true
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
@@ -42,41 +32,105 @@ window.addEventListener('resize', () => {
 
 const controls = new OrbitControls(camera, renderer.domElement)
 controls.enableDamping = true
-
-
-
-new GLTFLoader().load('models/suzanne_scene.glb', (gltf) => {
-	const suzanne = gltf.scene.getObjectByName('Suzanne') as THREE.Mesh
-	suzanne.castShadow = true
-
-	;((suzanne.material as THREE.MeshStandardMaterial).map as THREE.Texture).colorSpace = THREE.LinearSRGBColorSpace
-
-	// console.log(gltf.scene)
-
-	const plane = gltf.scene.getObjectByName('Plane') as THREE.Mesh
-	plane.receiveShadow = true
-
-	const spotLight = gltf.scene.getObjectByName('Spot') as THREE.SpotLight
-	spotLight.intensity /= 500
-	spotLight.castShadow = true
-
-	const textureLoader = new THREE.TextureLoader()
-	const textureFlare0 = textureLoader.load('https://cdn.jsdelivr.net/gh/Sean-Bradley/First-Car-Shooter@main/dist/client/img/lensflare0.png')
-
-	const lensflare = new Lensflare()
-	lensflare.addElement(new LensflareElement(textureFlare0, 1000, 0))
-	spotLight.add(lensflare)
-
-	scene.add(gltf.scene)
-})
+controls.target.set(0, 0.75, 0)
 
 const stats = new Stats()
 document.body.appendChild(stats.dom)
 
+function lerp(from: number, to: number, speed: number) {
+  const amount = (1 - speed) * from + speed * to
+  return Math.abs(from - to) < 0.001 ? to : amount
+}
+
+let mixer: THREE.AnimationMixer
+let animationActions: { [key: string]: THREE.AnimationAction } = {}
+let activeAction: THREE.AnimationAction
+let speed = 0,
+  toSpeed = 0
+
+// new GLTFLoader().load('models/eve$@walk.glb', (gltf) => {
+// 	  mixer = new THREE.AnimationMixer(gltf.scene)
+// 	  console.log(gltf)
+//
+// 	  mixer.clipAction(gltf.animations[0]).play()
+//
+// 	scene.add(gltf.scene)
+// })
+
+async function loadEve() {
+  const loader = new GLTFLoader()
+  const [eve, idle, run] = await Promise.all([
+    loader.loadAsync('models/eve$@walk.glb'),
+    loader.loadAsync('models/eve@idle.glb'),
+    loader.loadAsync('models/eve@run.glb')
+  ])
+
+  mixer = new THREE.AnimationMixer(eve.scene)
+
+  // mixer.clipAction(idle.animations[0]).play()
+
+  animationActions['idle'] = mixer.clipAction(idle.animations[0])
+  animationActions['walk'] = mixer.clipAction(eve.animations[0])
+  animationActions['run'] = mixer.clipAction(run.animations[0])
+
+  animationActions['idle'].play()
+  activeAction = animationActions['idle']
+
+  scene.add(eve.scene)
+}
+await loadEve()
+
+const keyMap: { [key: string]: boolean } = {}
+
+const onDocumentKey = (e: KeyboardEvent) => {
+  keyMap[e.code] = e.type === 'keydown'
+}
+document.addEventListener('keydown', onDocumentKey, false)
+document.addEventListener('keyup', onDocumentKey, false)
+
+const clock = new THREE.Clock()
+let delta = 0
+
 function animate() {
 	requestAnimationFrame(animate)
 
+	delta = clock.getDelta()
+
 	controls.update()
+
+	mixer.update(delta)
+
+	if (keyMap['KeyW']) {
+	  if (keyMap['ShiftLeft']) {
+	    //run
+	    if (activeAction != animationActions['run']) {
+	      activeAction.fadeOut(0.5)
+	      animationActions['run'].reset().fadeIn(0.25).play()
+	      activeAction = animationActions['run']
+	      toSpeed = 4
+	    }
+	  } else {
+	    //walk
+	    if (activeAction != animationActions['walk']) {
+	      activeAction.fadeOut(0.5)
+	      animationActions['walk'].reset().fadeIn(0.25).play()
+	      activeAction = animationActions['walk']
+	      toSpeed = 1
+	    }
+	  }
+	} else {
+	  //idle
+	  if (activeAction != animationActions['idle']) {
+	    activeAction.fadeOut(0.5)
+	    animationActions['idle'].reset().fadeIn(0.25).play()
+	    activeAction = animationActions['idle']
+	    toSpeed = 0
+	  }
+	}
+
+	speed = lerp(speed, toSpeed, delta * 10)
+	gridHelper.position.z -= speed * delta
+	gridHelper.position.z = gridHelper.position.z % 10
 
 	renderer.render(scene, camera)
 
